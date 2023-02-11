c8d5fc4aa9f5530785ec15a8877c98a68546e6d6
Evgeniy <andrej.burakov.91@mail.ru>
2023-02-11 23:05:38 +0300

    lesson5

---
+++ Lection/Lesson5/Program.cs
@@ -0,0 +1,64 @@
+﻿void DakArray(int[] raw)
+{
+    // int n = raw.Length;
+    // int i = 0;
+    // while(i < n)
+    // {
+    //     raw[i] = new Random().Next(10);
+    //     i++;
+    // }
+    for (int i = 0; i < raw.Length; i++)
+    {
+        raw[i] = new Random().Next(10);
+        System.Console.WriteLine(raw[i]);
+    }
+}
+
+// void DokArray(int[] row)
+// {
+//     // int m = row.Length;
+//     // int j = 0;
+//     // while(j < m)
+//     // {
+//     //     System.Console.WriteLine(row[j]);
+//     //     j++;
+//     // }
+//     for (int j = 0; j < row.Length; j++)
+//     {
+//         System.Console.WriteLine(row[j]);
+//     }
+// }
+int Bugaa(int[] law, int find)
+{
+    // int k = law.Length;
+    // int i = 0;
+    int index = 0;
+    // while(i < law.Length)
+    // {
+    //     if(law[i] == find)
+    //     {
+    //         index = i;
+    //         // break;
+    //     }
+    //     i++;
+    // }
+    for (int i = 0; i < law.Length; i++)
+    {
+         if(law[i] == find)
+        {
+            index = i;
+            // break;
+        }
+    }
+    return index; 
+}
+
+
+int[] array = new int[10];
+
+DakArray(array);
+// DokArray(array);
+System.Console.WriteLine();
+
+int ind = Bugaa(array, 4);
+System.Console.WriteLine(ind);
