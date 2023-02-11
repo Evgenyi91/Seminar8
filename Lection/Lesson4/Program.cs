c1a93f15521ec5119ccdae00a056d9b882291995
Evgeniy <andrej.burakov.91@mail.ru>
2023-02-11 21:40:57 +0300

    lesson4

---
+++ Lection/Lesson4/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = {1, 2, 4, 3, 5, 6, 7, 8, 9, 6};
+int n = array.Length;
+int find = 9;
+// int index = 0;
+// while(index < n)
+// {
+//     if(array[index] == find)
+//     {
+//         System.Console.WriteLine(index);
+//         // break;
+//     }
+//     index++;
+// }
+for(int index = 0; index < n; index++)
+{
+    if(array[index] == find)
+    // break;
+    System.Console.WriteLine(index);
+}
