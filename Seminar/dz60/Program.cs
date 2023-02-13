c898ca5fad115449b4ab2f71423e2cd8ca509482
Evgeniy <andrej.burakov.91@mail.ru>
2023-02-13 19:43:47 +0300

    60

---
+++ Seminar/dz60/Program.cs
@@ -0,0 +1,32 @@
+﻿int[,,] array = new int[2,2,2];
+// Random rnd = new Random();
+FillArray(array);
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        System.Console.WriteLine();
+       for (int k = 0; k < array.GetLength(2); k++)
+       {
+        // array[i,j,k] = rnd.Next(10, 99);
+       System.Console.Write($"{array[i,j,k]} ({i}{j}{k}) ");
+       } 
+    }
+}    
+void FillArray(int[,,] arr)
+{
+    int count = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
+
+
