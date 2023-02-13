3ddf7b127c5c41e64c403160c7277b647ab05049
Evgeniy <andrej.burakov.91@mail.ru>
2023-02-13 20:01:30 +0300

    62

--- Seminar/dz54/Program.cs
+++ Seminar/dz54/Program.cs
@@ -5,8 +5,6 @@ SortToLower(table);
 Console.WriteLine();
 PrintArray(table);
 
-
-// Функция заполнения массива рандомно числами от 1 до 9
 void FillArrayRandom(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -18,7 +16,6 @@ void FillArrayRandom(int[,] array)
     }
 }
 
-// Функция сортировки элементов в строке двумерного массива, по убыванию
 void SortToLower(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -38,7 +35,6 @@ void SortToLower(int[,] array)
     }
 }
 
-// Функция вывода двумерного массива
 void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
